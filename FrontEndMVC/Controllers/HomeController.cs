f2e756425b5ececca0c811960a3f854cfd053841
Luka Radulovic <lukaradulovic86@gmail.com>
2023-03-07 13:43:53 +0100

    Dodao sam momenat sa login sto bi simuliralo ulazak u tu aplikaciju koju radnici telekomunikacione kompanije koriste da nagradjuju klijente, doda osnovnu autentifikaciju preko formi

--- FrontEndMVC/Controllers/HomeController.cs
+++ FrontEndMVC/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using FrontEndMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace FrontEndMVC.Controllers
 {
@@ -28,6 +30,27 @@ namespace FrontEndMVC.Controllers
         }
 
         public ActionResult Login() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserBO user)
+        {
+            HttpContext.Session.Timeout = 30;
+            HttpContext.Session["Username"] = user.Username;
+            
+            HttpContext.Session["Password"] = user.Password;
+           
+            HttpCookie Autentifikacija;
+            Autentifikacija = FormsAuthentication.GetAuthCookie(user.Username, true);
+            Autentifikacija.Expires = DateTime.Now.AddHours(1);
+            Response.Cookies.Add(Autentifikacija);
+           
+            return RedirectToAction("MainMenu");
+        }
+
+        public ActionResult MainMenu() 
         {
             return View();
         }
