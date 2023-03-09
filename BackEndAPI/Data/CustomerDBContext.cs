d868c720ad236f5769494b843068a4156c91ab66
Luka Radulovic <lukaradulovic86@gmail.com>
2023-03-09 11:00:12 +0100

    Dodao konekcioni string, i u builder.service uneo novi DB context, takodje u klasi od DB contexta napravio konstruktor kako bi se parametar opcije prosledjivale i baznoj klasi

--- BackEndAPI/Data/CustomerDBContext.cs
+++ BackEndAPI/Data/CustomerDBContext.cs
@@ -5,6 +5,11 @@ namespace BackEndAPI.Data
 {
     public class CustomerDBContext:DbContext
     {
+        public CustomerDBContext(DbContextOptions<CustomerDBContext> options):base(options)
+        {
+            
+        }
+
         public DbSet<Customer> Customers {get; set; }    
     }
 }
--- BackEndAPI/Program.cs
+++ BackEndAPI/Program.cs
@@ -1,7 +1,13 @@
+using BackEndAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<CustomerDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
