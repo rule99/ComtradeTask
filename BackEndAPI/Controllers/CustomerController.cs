2f56fc2717219050b5a3276471d9987afb5434ba
Luka Radulovic <lukaradulovic86@gmail.com>
2023-03-09 10:31:50 +0100

    Dodao kontrolera i Biznis model za Customer

---
+++ BackEndAPI/Controllers/CustomerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEndAPI.Controllers
+{
+    [Route("api/Customer")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+    }
+}
---
+++ BackEndAPI/Model/CustomerBO.cs
@@ -0,0 +1,13 @@
+﻿namespace BackEndAPI.Model
+{
+    public class CustomerBO
+    {
+        public string Name { get; set; }
+
+        public DateTime Birth { get; set; }
+
+        public string SSN { get; set; }
+
+        public int ReturnCustomer { get; set; }
+    }
+}
