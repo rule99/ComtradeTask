7d89a7c292bd6c3f096a1756a5df3418a49177d1
Luka Radulovic <lukaradulovic86@gmail.com>
2023-03-09 11:05:49 +0100

    Izvrsio prvu migraciju/kreiranje baze koje ce API da koristi

---
+++ BackEndAPI/Migrations/20230309100215_KreiranjeBaze.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackEndAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class KreiranjeBaze : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Birth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SSN = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    ReturnCustomer = table.Column<int>(type: "int", nullable: false),
+                    DateRewarded = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AgentUserName = table.Column<string>(type: "nvarchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
