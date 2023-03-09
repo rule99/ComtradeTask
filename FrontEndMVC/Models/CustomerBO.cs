5661e7ca1832ea54ea360e384ac7c6c6347d9c3a
Luka Radulovic <lukaradulovic86@gmail.com>
2023-03-09 14:09:08 +0100

    #2

--- FrontEndMVC/Models/CustomerBO.cs
+++ FrontEndMVC/Models/CustomerBO.cs
@@ -7,5 +7,16 @@ namespace FrontEndMVC.Models
 {
     public class CustomerBO
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birth { get; set; }
+
+        public string SSN { get; set; }
+
+        public HomeBO Home { get; set; }
+
+        public int ReturnCustomer { get; set; }
     }
 }
--- FrontEndMVC/Models/HomeBO.cs
+++ FrontEndMVC/Models/HomeBO.cs
@@ -7,5 +7,12 @@ namespace FrontEndMVC.Models
 {
     public class HomeBO
     {
+        public int Id { get; set; }
+
+        public string Adress { get; set; }
+
+        public string City { get; set; }
+
+        public string Zip { get; set; }
     }
 }
